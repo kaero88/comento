bf2ce2f613f4f1557506b662d163b37713db41fa
s <jh.shin@muzlive.com>
2022-12-01 21:29:07 +0900

    comento-1201

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return {'message': 'Hello Flask World'}
+    return {'message': '-Hello Flask World-'}
     
 if __name__ == '__main__':
     app.run()
